a6c6a145ca30a7851e17618a401e47ba1ba9970e
ia0 <github@ia0.eu>
2011-10-22 12:03:24 +0200

    test mode

--- src/main.cc
+++ src/main.cc
@@ -1,19 +1,27 @@
 #include <iostream>
 
+#include <QApplication>
+#include <QProcessEnvironment>
+
 #include "term.hh"
 
-int main() {
+int main (int argc, char *argv[]) {
   using namespace std ;
-  using namespace term ;
-  for (unsigned int i = 0 ; i < 50 ; ++i) {
-    Term *term = randTerm(i) ;
-    bool loop = true ;
-    while (loop) {
-      term->print() ;
-      loop = reduce(term) ;
+  QProcessEnvironment env = QProcessEnvironment::systemEnvironment() ;
+  if (env.contains(QString("TEST_MODE"))) {
+    using namespace term ;
+    for (unsigned int i = 0 ; i < 50 ; ++i) {
+      Term *term = randTerm(i) ;
+      bool loop = true ;
+      while (loop) {
+        term->print() ;
+        loop = reduce(term) ;
+      }
+      delete term ;
+      cout << endl ;
     }
-    delete term ;
-    cout << endl ;
+    return 0 ;
   }
-  return 0 ;
+  QApplication app(argc, argv) ;
+  return app.exec() ;
 }
